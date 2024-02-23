7d55916c10dc95749097c988c83a8f94665571b5
Sami <sami.perrin@chuv.ch>
2024-02-23 16:52:02 +0100

    uncomment test

--- ___tests__/Home.test.tsx
+++ ___tests__/Home.test.tsx
@@ -12,7 +12,7 @@ jest.mock('../src/utils/AuthContext', () => ({
   logout: jest.fn(),
 });
 
-xdescribe('Home', () => {
+describe('Home', () => {
   it('heading', () => {
     render(<Home />)
     const heading = screen.getByRole('heading', { level: 2, name: /Welcome to The Frontend App/i });
