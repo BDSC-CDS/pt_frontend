b2782953fe0d00749deac3797c5b53604231cd48
kobikacc <148439855+kobikacc@users.noreply.github.com>
2024-08-29 16:36:19 +0200

    changed the button design

--- src/pages/dataset.tsx
+++ src/pages/dataset.tsx
@@ -217,11 +217,16 @@ export default function Dataset() {
                         <h1 className="text-3xl font-bold">Datasets</h1>
                         <button
                             onClick={() => setIsUploadModalOpen(true)}
-                            className="bg-gray-200 hover:bg-gray-300 p-2 pr-3 rounded cursor-pointer flex items-center"
+                            className="text-white bg-[#306278] hover:bg-[#255362] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2"
                         >
-                            <MdOutlineAdd size={20} />
-                            <span className='ml-2 text-sm'>New project</span>
+                            <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                            New project
                         </button>
+
+
+
                     </div>
                     <Modal show={isUploadModalOpen} onClose={() => closeUploadModals()}>
                         <Modal.Body>
