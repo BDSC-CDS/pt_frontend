de86f6929da8eb67d7d4711e397c5b87fdbea51a
bkaabachi <mohamed-beyrem.kaabachi@chuv.ch>
2025-01-06 16:28:23 +0000

    changes to sidebar UI and main page

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,7 +1,7 @@
 import { Sidebar } from 'flowbite-react';
 import Link from 'next/link';
 import { BiBuoy } from 'react-icons/bi';
-import { HiArrowSmRight, HiDatabase, HiInbox, HiShoppingBag, HiPresentationChartBar, HiOutlineCog, HiLockClosed } from 'react-icons/hi';
+import { HiArrowSmRight, HiDatabase, HiInbox, HiShoppingBag, HiPresentationChartBar, HiOutlineCog, HiLockClosed, HiCalculator, HiShieldCheck  } from 'react-icons/hi';
 import { HiClipboardDocumentList } from 'react-icons/hi2';
 import { useAuth } from '~/utils/authContext';
 import type { CustomFlowbiteTheme } from "flowbite-react";
@@ -21,41 +21,42 @@ export default function SideMenu() {
     return (
         // <div className="fixed top-20 left-0 h-3/4 z-10 text-neutral-950">
         <div className="w-75 h-100 top-14 sticky h-full bg-white">
-
-            <Sidebar aria-label="Sidebar with content separator example" className="w-90 border pt-10 rounded">
-                <div>
-                    <Sidebar.Items>
-
-                        <Sidebar.Collapse icon={HiPresentationChartBar} theme={customTheme.collapse} label="Risk Assessment">
-                                <Link href="/risk_assessment" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
-                                    <HiPresentationChartBar />
-                                    <p className='ml-1'> Qualitative Risk Assessment</p>
-                                </Link>
-                                <Link href="#" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
-                                    <HiPresentationChartBar />
-                                    <p className='ml-1'> Quantitative Risk Assessment</p>
-                                </Link>
-                        </Sidebar.Collapse>
-
-                        <Sidebar.ItemGroup>
+            <Sidebar aria-label="Sidebar with content separator example" className="mt-4 w-90 border pt-10 rounded">
+                <Sidebar.Items>
+                    <Sidebar.ItemGroup>
                             <Link href="/dataset" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
                                 <HiDatabase />
-                                <p className='ml-1'> Datasets</p>
+                                <p className='ml-1'> My Datasets</p>
                             </Link>
-                            <Link href="#" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
-                                <HiShoppingBag />
-                                <p className='ml-1'> Tabular DeID </p>
+                    </Sidebar.ItemGroup> 
+                    <Sidebar.ItemGroup>
+                            <Link href="/dataset" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
+                                <HiPresentationChartBar />
+                                <p className='ml-1'> Qualitative Assessment</p>
                             </Link>
-                            <Link href="#" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
-                                <HiShoppingBag />
-                                <p className='ml-1'> Synthetic Data Generation</p>
+                             <Link href="/dataset" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
+                                <HiPresentationChartBar />
+                                <p className='ml-1'> Quantitative Assessment</p>
                             </Link>
-                            <Link href="#" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
-                                <HiShoppingBag />
-                                <p className='ml-1'> Text DeID</p>
+                    </Sidebar.ItemGroup> 
+                    <Sidebar.ItemGroup>
+                            <Link href="/dataset" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
+                                <HiShieldCheck />
+                                <p className='ml-1'> Rule-Based de-identification</p>
                             </Link>
-                        </Sidebar.ItemGroup>
-                        <Sidebar.ItemGroup className="mt-10">
+                             <Link href="/dataset" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
+                                <HiShieldCheck />
+                                <p className='ml-1'> Formal de-identification</p>
+                            </Link>
+                            <Link href="/" passHref className='flex items-center ml-2 hover:bg-gray-100 hover:rounded'>
+                                <HiShieldCheck />
+                                <p className='ml-1'> Synthetic Data</p>
+                            </Link>
+                    </Sidebar.ItemGroup> 
+
+
+                    <Sidebar.ItemGroup>
+
                             <Sidebar.Collapse className={`${!isAdmin ? "hidden" : ""}`} icon={HiOutlineCog} theme={customTheme.collapse} label="Admin">
                                 <Link href="/admin/questionnaire" passHref className='flex items-center ml-10 hover:bg-gray-100 hover:rounded'>
                                     <HiClipboardDocumentList />
@@ -74,9 +75,9 @@ export default function SideMenu() {
                                 <BiBuoy />
                                 <p className='ml-1'> Settings</p>
                             </Link>
-                        </Sidebar.ItemGroup>
-                    </Sidebar.Items>
-                </div>
+
+                    </Sidebar.ItemGroup> 
+                </Sidebar.Items>
             </Sidebar>
         </div>
     )
