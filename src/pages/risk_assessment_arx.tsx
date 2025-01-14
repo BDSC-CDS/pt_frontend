1657520d25f2d780a8c09b98ee2d3b62a1c47f05
Sami <5501101+sami-sweng@users.noreply.github.com>
2025-01-14 10:40:07 +0100

    Clarify/rename transform, improve pages structure, add dataset selector, use it (#54)
    * rename, add text, improve dataset base page
    
    * revert change url
    
    * update icons, use selector generally, update dataset actions
    
    * dataset page add option, fix disabled links
    
    * Update src/pages/deidentification-notebook.tsx
    
    Co-authored-by: Jérémie Frei <jeremie.frei@chuv.ch>
    
    * Update src/pages/dataset/[id]/index.tsx
    
    Co-authored-by: Jérémie Frei <jeremie.frei@chuv.ch>
    
    ---------
    
    Co-authored-by: Jérémie Frei <jeremie.frei@chuv.ch>

--- src/pages/risk_assessment_arx.tsx
+++ src/pages/risk_assessment_arx.tsx
@@ -1,56 +1,13 @@
 
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { listDatasets } from "../utils/dataset"
-import { useEffect, useState } from 'react';
-import { TemplatebackendDataset } from '~/internal/client';
 import { useAuth } from '~/utils/authContext';
 import { getRiskAssessment } from '~/utils/RiskAssessmentArx';
-import DataTable from '~/components/DataTable';
+import DatasetSelector from '~/components/DatasetSelector';
+
 
 export default function RiskAssessmentArx() {
-    // Authentication
     const { isLoggedIn } = useAuth();
 
-    // Routing
-    const router = useRouter();
-
-    // States
-    const [datasetsList, setDatasetsList] = useState<Array<TemplatebackendDataset>>([]);
-
-    const getListDatasets = async (offset?: number, limit?: number) => {
-        // Call API
-        let response;
-        if (!offset && !limit) {
-            response = await listDatasets();
-        } else if (offset && limit) {
-            response = await listDatasets(
-            );
-        } else {
-            console.log("ERROR You have to define both the offset and the limit") // TODO
-            return;
-        }
-        const result = response?.result?.datasets
-        if (result) {
-            setDatasetsList(result);
-        }
-    }
-
-    useEffect(() => {
-        try {
-            getListDatasets();
-        } catch (error) {
-            alert("Error listing the datasets")
-        }
-    }, []);
-
-   
-    // Event handlers
-    const handleRowClick = (id: number | undefined) => {
-        if (id) {
-            router.push(`/dataset/${id}`);
-        }
-    };
 
     const handleRiskAssessment = async (id: number | undefined) => {
         if (id) { 
@@ -91,24 +48,21 @@ export default function RiskAssessmentArx() {
                     <div className="flex justify-between items-center mb-4">
                         <h1 className="text-3xl font-bold">Quantitative Risk Assessment</h1>
                     </div>
-                    {/* Dataset table */}
-                    {datasetsList.length > 0 ? (
-                        <DataTable 
-                            data={datasetsList.map(dataset => ({...dataset, datasetQuasiIdentifiers:"Not yet implemented"}))}
-                            columns={[
-                                {name:"id", header:"Dataset ID"},
-                                {name:"datasetName", header:"Dataset Name"},
-                                {name:"datasetQuasiIdentifiers", header: "Quasi-identifiers"},
-                                {name:"createdAt", header:"Created At"},
-                            ]}
-                            onRowClick={(row) => handleRowClick(row.id)}
-                            actions={[
-                                {name:"Risk Assessment", callback: (row) => handleRiskAssessment(row.id)},
-                            ]}
-                        />
-                    ) : (
-                        <div className="text-center text-gray-500 mt-20">No datasets yet</div>
-                    )}
+                    <div className="flex justify-between items-center mb-4">
+                        <p>
+                            On this page you are able to formally assert the risk of reidetification of your dataset by using ARX.
+                        </p>
+                    </div>
+                    <div className="flex justify-between items-center mb-4">
+                        <p className="font-bold">Please select a dataset to start</p>
+                    </div>
+
+                    <DatasetSelector
+                        // onRowClick={(id) => handleRowClick(id)}
+                        actions={[
+                            { name: "Risk assesment", callback: (id) => handleRiskAssessment(id) },
+                        ]}
+                    />
                 </div>
             )}
         </>
