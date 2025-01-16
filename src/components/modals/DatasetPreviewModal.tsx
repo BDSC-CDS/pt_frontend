a756e39eb7402949f42f217f03a303355cd6828a
Jeremie Frei <jeremie.frei@chuv.ch>
2025-01-16 11:16:26 +0000

    Add dataset preview table and loading state

--- src/components/modals/DatasetPreviewModal.tsx
+++ src/components/modals/DatasetPreviewModal.tsx
@@ -1,9 +1,9 @@
-import { Button, Modal } from 'flowbite-react';
-import { useRouter } from 'next/router';
-import { ReactNode, useEffect, useState } from 'react';
-import { TemplatebackendColumn, TemplatebackendMetadata } from '~/internal/client';
-import { getDatasetContent, getInfo, getMetadata } from '~/utils/dataset';
-import DataTable from '../DataTable';
+import { Button, Modal } from "flowbite-react";
+import { ReactNode, useEffect, useState } from "react";
+import { TemplatebackendMetadata } from "~/internal/client";
+import { getDatasetContent, getInfo, getMetadata } from "~/utils/dataset";
+import DataTable from "../DataTable";
+import Spinner from "../ui/Spinner";
 
 interface DatasetPreviewModalProps {
     show: boolean
@@ -21,78 +21,93 @@ interface DatasetInfo {
     columns: { name: string; header: string; tooltip?: ReactNode; }[]
 }
 
-export default function DatasetPreviewModal({show, datasetId, onClose}: DatasetPreviewModalProps) {
-    const router = useRouter()
+// Adjust the number of rows to preview here
+const PREVIEW_NB_ROWS = 5
 
-    const [datasetInfo, setDatasetInfo] = useState<DatasetInfo>({
-        name: "",
-        createdAt: undefined,
-        colNumber: 0,
-        rowNumber: 0,
-        metadata: [],
-        data: [],
-        columns: [],
-    })
+export default function DatasetPreviewModal({show, datasetId, onClose}: DatasetPreviewModalProps) {
+    const [isLoading, setIsLoading] = useState(true)
+    const [datasetInfo, setDatasetInfo] = useState<DatasetInfo>()
 
     const getDatasetInfo = async () => {
-        const response1 = await getInfo(datasetId);
-        if (!response1?.dataset) {
-            throw new Error("Dataset not found.")
-        }
+        try {
+            setIsLoading(true)
 
-        const response2 = await getMetadata(datasetId)
-        if (!response2?.metadata) {
-            throw new Error("Metadata not found.")
+            const response1 = await getInfo(datasetId);
+            if (!response1?.dataset) {
+                throw new Error("Dataset not found.")
+            }
+
+            const datasetName = response1.dataset.datasetName
+            const datasetCreatedAt = response1.dataset.createdAt
+
+            const response2 = await getMetadata(datasetId)
+            if (!response2?.metadata) {
+                throw new Error("Metadata not found.")
+            }
+
+            const datasetMetadata = response2.metadata.metadata
+            const datasetColNumber = datasetMetadata?.length
+            
+            const response3 = await getDatasetContent(datasetId, 0, PREVIEW_NB_ROWS)
+            if (!response3?.result || !response3?.result?.columns) {
+                throw new Error("Dataset content not found.")
+            }
+
+            const datasetRowNumber = response3.result.nRows
+
+            // Process dataset columns
+            const datasetRows = getDataFromColumns(
+                response3.result?.columns.map(c => c.value) ?? [], 
+                datasetMetadata?.map(m => m.columnName) ?? []
+            )
+            
+            // Process dataset metadata
+            const datasetColumns = datasetMetadata? datasetMetadata.map((meta, index) => ({
+                name: `column${meta.columnId}`,
+                header: meta.columnName || `column${index}`,
+                tooltip: (
+                    <div className="text-sm" style={{ textTransform: "none" }}>
+                        <p><strong>Type:</strong> {meta.type}</p>
+                        <p><strong>Identifier:</strong> {meta.identifier}</p>
+                        <p><strong>Is the ID Column:</strong> {meta.isId ? "Yes" : "No"}</p>
+                    </div>
+                )
+            }))
+            : [];
+
+            // Construct new dataset info object
+            const newDatasetInfo: DatasetInfo = {
+                name: datasetName || "",
+                createdAt: datasetCreatedAt || undefined,
+                colNumber: datasetColNumber || 0,
+                rowNumber: datasetRowNumber || 0,
+                metadata: datasetMetadata || [],
+                data: datasetRows,
+                columns: datasetColumns
+            }
+
+            // Update state
+            setDatasetInfo(newDatasetInfo)
+        } catch (error) {
+            console.error("Error fetching the dataset info:", error)
+            throw error
+        } finally {
+            setIsLoading(false)
         }
         
-        const response3 = await getDatasetContent(datasetId, 0, 5)
-        if (!response3?.result) {
-            throw new Error("Dataset content not found.")
-        }
-
-        const datasetName = response1.dataset.datasetName
-        const datasetCreatedAt = response1.dataset.createdAt
-        const datasetMetadata = response2.metadata.metadata
-        const datasetColNumber = datasetMetadata?.length
-        const datasetRowNumber = response3.result.nRows
-        // const datasetRows = getDataFromColumns(response3.result.columns!.map(c => c.value), datasetMetadata!.map(m => m.columnName))
-        const datasetColumns = datasetMetadata? datasetMetadata.map((meta, index) => ({
-              name: `column${meta.columnId}`,
-              header: meta.columnName || `column${index}`,
-              tooltip: (
-                  <div className="text-sm" style={{ textTransform: 'none' }}>
-                      <p><strong>Type:</strong> {meta.type}</p>
-                      <p><strong>Identifier:</strong> {meta.identifier}</p>
-                      <p><strong>Is the ID Column:</strong> {meta.isId ? 'Yes' : 'No'}</p>
-                  </div>
-              )
-          }))
-        : [];
-
-        const newDatasetInfo: DatasetInfo = {
-            name: datasetName || "",
-            createdAt: datasetCreatedAt || undefined,
-            colNumber: datasetColNumber || 0,
-            rowNumber: datasetRowNumber || 0,
-            metadata: datasetMetadata || [],
-            data: [],
-            columns: datasetColumns
-        }
-
-        setDatasetInfo(newDatasetInfo)
     }
 
-    // const getDataFromColumns = (columns:(string[] | undefined)[], metadata: (string | undefined)[]) => {
-    //     const rows: Record<string, any>[] = Array.from({length: 5}, (_, rowIndex) => {
-    //         const row: Record<string, any> = {}
-    //         metadata.forEach((meta, colIndex) => {
-    //             row[meta!] = columns[rowIndex]?.[colIndex]
-    //         })
-    //         return row
-    //     })
+    const getDataFromColumns = (columns:(string[] | undefined)[], metadata: (string | undefined)[]) => {
+        const rows: Record<string, any>[] = Array.from({length: columns[0]!.length}, (_, rowIndex) => {
+            const row: Record<string, any> = {}
+            metadata.forEach((meta, colIndex) => {
+                row[`column${colIndex}`] = columns[colIndex]?.[rowIndex]
+            })
+            return row
+        })
 
-    //     return rows
-    // }
+        return rows
+    }
 
     useEffect(() => {
         if (show) {
@@ -102,31 +117,45 @@ export default function DatasetPreviewModal({show, datasetId, onClose}: DatasetP
                 alert("Error retrieving the dataset info.")
             }
         }
-    }, [show, datasetId]);
-    
+    }, [show]);
+
+    // Handlers
+    const handleCloseModal = () => {
+        setIsLoading(true)
+        onClose()
+    }
+
     return (
         <>
-            <Modal show={show} onClose={onClose} size="4xl">
+            
+
+            <Modal show={show} onClose={handleCloseModal} size="7xl">
                 <Modal.Header/>
                 <Modal.Body>
-                    <div className="bg-gray-100 p-5 rounded-lg shadow text-md ">
-                        <h2 className="text-lg font-bold mb-2"> {datasetInfo.name}</h2>
-                        <p><strong>Created Date:</strong> {datasetInfo.createdAt?.toLocaleDateString()} </p>
-                        <p><strong>Number of Rows:</strong> {datasetInfo.rowNumber}</p>
-                        <p><strong>Number of Columns:</strong> {datasetInfo.colNumber} </p>
-                    </div>
-                    <br />
-                    {/* Dataset table view */}
-                    {datasetInfo && (
-                        <DataTable
-                            data={datasetInfo.data}
-                            columns={datasetInfo.columns}
-                        />
-                    )}
-                    
+                    {isLoading ? (<Spinner/>) : (
+                        <div className="flex flex-col gap-5">
+                            {/* Dataset Info */}
+                            {datasetInfo && (
+                                <div className="bg-gray-50 p-5 rounded-lg border text-md ">
+                                    <h2 className="text-lg font-bold mb-2"> {datasetInfo.name}</h2>
+                                    <p><strong>Created Date:</strong> {datasetInfo.createdAt?.toLocaleDateString()} </p>
+                                    <p><strong>Number of Rows:</strong> {datasetInfo.rowNumber}</p>
+                                    <p><strong>Number of Columns:</strong> {datasetInfo.colNumber} </p>
+                                </div>
+                            )}
+                            
+                            {/* Dataset table view */}
+                            {datasetInfo && (
+                                <DataTable
+                                    data={datasetInfo.data}
+                                    columns={datasetInfo.columns}
+                                />
+                            )}
+                        </div> 
+                    )}                   
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={onClose}>Close</Button>
+                    <Button onClick={handleCloseModal}>Close</Button>
                 </Modal.Footer>
             </Modal>
         </>
