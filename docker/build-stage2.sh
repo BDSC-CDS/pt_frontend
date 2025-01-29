8f424e89eef128995011b10869d553c89ddcb93a
Sami <sami.perrin@chuv.ch>
2025-01-29 20:19:36 +0100

    add package step

--- docker/build-stage2.sh
+++ docker/build-stage2.sh
@@ -2,5 +2,12 @@
 
 set -e
 
-docker build --pull -f dockerfiles/stage2.dockerfile -t registry.rdeid.unil.ch/pt-frontend:latest ..
-docker push registry.rdeid.unil.ch/pt-frontend:latest
+docker build --pull -f dockerfiles/stage2.dockerfile -t registry.rdeid.unil.ch/pt-frontend:${IMAGE_TAG} ..
+docker push registry.rdeid.unil.ch/pt-frontend:${IMAGE_TAG}
+
+
+if [[ ${IMAGE_TAG} =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    docker tag registry.rdeid.unil.ch/pt-frontend:${IMAGE_TAG} ghcr.io/bdsc-cds/pt-frontend:${IMAGE_TAG}
+    docker push ghcr.io/bdsc-cds/pt-frontend:${IMAGE_TAG}
+fi
+
