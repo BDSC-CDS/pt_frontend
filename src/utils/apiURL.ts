e1176880c887a84afc9d051ec51b86b4a6b59e00
Jeremie Frei <jeremie.frei@chuv.ch>
2025-01-30 12:33:25 +0000

    Add log message for used API url

--- src/utils/apiURL.ts
+++ src/utils/apiURL.ts
@@ -1,4 +1,5 @@
 export const apiURL = process.env.API_URL ||"https://pt-backend.rdeid.unil.ch/"; // TODO: Modify default api url
+console.log("Using API at ", apiURL)
 // export const apiURL = "https://pt-backend-test.rdeid.unil.ch/";
 // export const apiURL = "https://pt-backend.rdeid.unil.ch/";
 // export const apiURL = "http://localhost:5000";
