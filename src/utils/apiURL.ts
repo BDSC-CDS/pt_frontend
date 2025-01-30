bcba5b46f2acc237fcf388168dd02cb0bc0d20ef
Jeremie Frei <jeremie.frei@chuv.ch>
2025-01-30 12:29:35 +0000

    Use API_URL env variable if present

--- src/utils/apiURL.ts
+++ src/utils/apiURL.ts
@@ -1,2 +1,4 @@
-export const apiURL = "https://pt-backend.rdeid.unil.ch/";
+export const apiURL = process.env.API_URL ||"https://pt-backend.rdeid.unil.ch/"; // TODO: Modify default api url
+// export const apiURL = "https://pt-backend-test.rdeid.unil.ch/";
+// export const apiURL = "https://pt-backend.rdeid.unil.ch/";
 // export const apiURL = "http://localhost:5000";
