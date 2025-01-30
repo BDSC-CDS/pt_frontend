6bfb817c4a4ef47fa888dd459b7886b170b48c93
Jérémie Frei <jeremie.frei@chuv.ch>
2025-01-30 15:50:13 +0100

    Fix using entrypoint and public file for apiUrl

---
+++ docker/dockerfiles/entrypoint.sh
@@ -0,0 +1,2 @@
+echo '{"apiUrl": "' $API_URL '"}' > /usr/share/nginx/html/apiUrl.json
+nginx -g daemon off
--- src/utils/apiURL.ts
+++ src/utils/apiURL.ts
@@ -1,4 +1,6 @@
-export const apiURL = process.env.API_URL ||"https://pt-backend.rdeid.unil.ch/"; // TODO: Modify default api url
+import apiUrl from "public/apiURL.json"
+
+export const apiURL = apiUrl;
 console.log("Using API at ", apiURL)
 // export const apiURL = "https://pt-backend-test.rdeid.unil.ch/";
 // export const apiURL = "https://pt-backend.rdeid.unil.ch/";
