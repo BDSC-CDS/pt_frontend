53cc0effbdf6db0ab5d0fb5d8b92e1dbcbd93625
Jérémie Frei <jeremie.frei@chuv.ch>
2025-02-14 17:16:45 +0100

    Add custom tooltip for DataTable actions

---
+++ src/components/ui/CustomTooltip.tsx
@@ -0,0 +1,57 @@
+import ReactDOM from 'react-dom';
+import { useState, useRef, useEffect, ReactNode, FC } from 'react';
+
+interface CustomTooltipProps {
+    content: ReactNode;
+    children: ReactNode;
+    className?: string;
+}
+
+export const CustomTooltip: FC<CustomTooltipProps> = ({ content, children, className }) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const [tooltipStyles, setTooltipStyles] = useState<{ top: number; left: number }>({ top: 0, left: 0 });
+    const triggerRef = useRef<HTMLDivElement>(null);
+    const tooltipRef = useRef<HTMLDivElement>(null);
+
+
+    const handleMouseEnter = () => {
+        if (triggerRef.current) {
+            const { top, left, width, height } = triggerRef.current.getBoundingClientRect();
+            console.log(top, left, width, height);
+            setTooltipStyles({
+                top: top + window.scrollY + 3*height/4, // Position below the trigger element
+                left: left + window.scrollX + width/2, // Center horizontally
+            });
+        }
+        setIsVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsVisible(false);
+    }
+
+    return (
+        <>
+            <div 
+                ref={triggerRef}
+                onMouseEnter={handleMouseEnter} 
+                onMouseLeave={handleMouseLeave}
+                className={className}
+            >
+                {children}
+            </div>
+            {isVisible && ReactDOM.createPortal(
+                <div 
+                    ref={tooltipRef}
+                    style={{ position: 'absolute', top: tooltipStyles.top, left: tooltipStyles.left, transform: 'translateX(-75%)' }} 
+                    className="fixed z-50 w-40"
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    {content}
+                </div>,
+                document.body
+            )}
+        </>
+    );
+};
