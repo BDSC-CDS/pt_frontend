0948033c8699483f249f644cdaba68f1c028bb70
Jérémie Frei <jeremie.frei@chuv.ch>
2025-02-21 15:16:47 +0100

    Fix logo_file_name env var to use

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 echo 'window.__APP_CONFIG__ = {API_URL: "'$API_URL'"};' > /usr/share/nginx/html/config.js
 
 # Copy the logo & CSS theme files
-cp -f /usr/share/nginx/html/$LOGO_FILE_NAME.png /usr/share/nginx/html/pt-logo.png
+cp -f /usr/share/nginx/html/$LOGO_FILE_NAME /usr/share/nginx/html/pt-logo.png
 cp -f /usr/share/nginx/html/css/$CSS_THEME.css /usr/share/nginx/html/css/pt-theme.css
 
 # Start Nginx
