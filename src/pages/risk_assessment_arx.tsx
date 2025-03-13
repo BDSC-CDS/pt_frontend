d07ef47345cdac50848f879d38dd5fae72def102
Jérémie Frei <jeremie.frei@chuv.ch>
2025-03-13 10:56:14 +0100

    Add download to dataset selector

--- src/pages/risk_assessment_arx.tsx
+++ src/pages/risk_assessment_arx.tsx
@@ -54,6 +54,7 @@ function RiskAssessmentArx() {
                 <DatasetSelector
                     onRowClick={handleRowClick}
                     preview
+                    download
                 />
             </div>
         </>
